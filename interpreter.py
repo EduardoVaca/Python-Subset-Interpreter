4090fde1e80d78aca898fb831188dfc8495395ca
EduardoVaca <vacalalo@gmail.com>
2017-09-11 21:34:51 +0200

    Add first lexer tokens

---
+++ interpreter.py
@@ -0,0 +1,61 @@
+# -------------------------------------
+# COMPILERS PROJECT: Python Interpreter
+# Carlos Eduardo Vaca Guerra
+# A01207563
+# -------------------------------------
+
+# List of all possible tokens allowed in my interpreter.
+tokens = (
+    'NAME', 'NUMBER',
+    'COMMENT',
+    'EQ', 'NEQ', 'GT', 'GE', 'LT', 'LE',
+    'PLUS', 'MINUS', 'PROD', 'DIV', 'EQUALS'
+)
+
+# Token definition with regex.
+t_PLUS      = r'\+'
+t_MINUS     = r'-'
+t_PROD      = r'\*'
+t_DIV       = r'/'
+t_EQUALS    = r'='
+t_EQ        = r'=='
+t_NEQ       = r'!='
+t_GT        = r'>'
+t_GE        = r'>='
+t_LT        = r'<'
+t_LE        = r'<='
+t_COMMENT   = r'\#'
+t_NAME      = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+def t_NUMBER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+# Ignored characters
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+import ply.lex as lex
+lexer = lex.lex()
+
+while True:
+    try:
+        s = input('Vaca > ')
+    except EOFError:
+        break
+    lexer.input(s)
+    for token in lexer:
+        print(token)
