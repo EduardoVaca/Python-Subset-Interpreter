bf1c142666ab50f09c7f62e5658317b6571c2e18
EduardoVaca <vacalalo@gmail.com>
2017-09-13 01:10:23 +0200

    Add helper function in tests

--- tests.py
+++ tests.py
@@ -1,13 +1,28 @@
+"""This module provides all the unit testing tools."""
 import unittest
 import interpreter
 
-lexer = interpreter.lexer
 
-class TestTry(unittest.TestCase):
+LEXER = interpreter.lexer
 
-    def test_try(self):
-        lexer.input('hi')        
-        self.assertEqual(lexer.token().type, 'ID')
+class TestLexer(unittest.TestCase):
+    """Class that tests all cases for my Subset Python lexer.
+    Checks every token that should be allowed.
+    """
+
+    def test_one_word_comment(self):
+        """Tests that one word comments is tokenized correctly.
+        """
+        LEXER.input('# word')        
+        self.checks_tokens(['COMMENT', 'ID'])
+    
+    def checks_tokens(self, correct_token_list):
+        """Checks that the tokens obtained by lexer are the expected.
+        PARAMS:
+        correct_token_list -- list of ordered tokens that should be obtained by lexer.
+        """
+        for tok in correct_token_list:
+            self.assertEqual(LEXER.token().type, tok)
 
 if __name__ == '__main__':
     unittest.main()
